d8cddd8b6fa2ecbee33c6c2615df29e80d4dfdc2
nikitasthakur <nikitathakur2211@gmail.com>
2022-03-02 00:05:33 -0600

    Question 1,2,3 and 5

--- code/read_data.py
+++ code/read_data.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
+import os
+import scipy
 import re
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
+path = os.getcwd()
 
 def read_for_crf(filename):
     data = []
@@ -109,6 +112,166 @@ def prepare_structured_dataset(filename, return_ids=False):
         return X_dataset, y_dataset
     else:
         return X_dataset, y_dataset, ids
+
+#Function for loading Q2 data
+def crf_dataLoader():
+    training_data,testing_data = OrderedDict(),OrderedDict()
+    
+    with open(path + '/data/train.txt', 'r') as file:
+        training_dataset = file.readlines()
+
+
+    for line in training_dataset:
+        character = re.findall('[a-z]', str(line))
+        line = re.findall('\d+', str(line))
+        character_id = int(line[0])
+        next_word = line[1]
+        word_id = line[2]
+        position = line[3]
+        pixel_value = np.array(line[4:], dtype=np.float32)
+        training_data[character_id] = [character, next_word, word_id, position, pixel_value]
+
+    
+    with open(path + '/data/test.txt', 'r') as file:
+        testing_dataset = file.readlines()
+
+    for line in testing_dataset:
+        character = re.findall(r'[a-z]', line)
+        line = re.findall(r'\d+', line)
+        character_id = line[0]
+        next_word = line[1]
+        word_id = line[2]
+        position = line[3]
+        pixel_value = np.array(line[4:], dtype=np.float32)
+        testing_data[character_id] = [character, next_word, word_id, position, pixel_value]
+
+    return training_data, testing_data
+
+def crf_dataLoader_distort(training_dataset, testing_dataset):
+    
+    training_data,testing_data = OrderedDict(),OrderedDict()
+    
+    for line in training_dataset:
+        character = re.findall('[a-z]', str(line))
+        line = re.findall('\d+', str(line))
+        character_id = int(line[0])
+        next_word = line[1]
+        word_id = line[2]
+        position = line[3]
+        pixel_value = np.array(line[4:], dtype=np.float32)
+        training_data[character_id] = [character, next_word, word_id, position, pixel_value]
+
+    
+    with open(path + '/data/test.txt', 'r') as file:
+        testing_dataset = file.readlines()
+
+    for line in testing_dataset:
+        character = re.findall(r'[a-z]', line)
+        line = re.findall(r'\d+', line)
+        character_id = line[0]
+        next_word = line[1]
+        word_id = line[2]
+        position = line[3]
+        pixel_value = np.array(line[4:], dtype=np.float32)
+        testing_data[character_id] = [character, next_word, word_id, position, pixel_value]
+
+    return training_data, testing_data
+
+
+def linearSVM_dataLoader(training_dataset, limit):
+    if limit == 0:
+        return training_dataset
+
+    word_dict = defaultdict(list)
+
+    for key, value in training_dataset.items():
+        word_id = value[2]
+        word_dict[word_id].append(key)
+
+    with open(path + '/data/transform.txt', 'r') as f:
+        lines = f.readlines()
+
+    lines = lines[:limit]
+
+    for line in lines:
+        splits = line.split()
+        action = splits[0]
+        target_word = splits[1]
+        args = splits[2:]
+
+        # get all of the ids in train set which have this word in them
+        target_image_ids = word_dict[target_word]
+
+        for image_id in target_image_ids:
+            value_set = training_dataset[image_id]
+            image = value_set[-1]
+
+            if action == 'r':
+                alpha = args[0]
+                image = rotate_image(image, alpha)
+            else:
+                offsets = args
+                image = translate_image(image, offsets)
+
+            value_set[-1] = image.flatten()
+
+            training_dataset[image_id] = value_set
+
+    return training_dataset
+
+
+#helper functions for data conversion
+
+# returns characters from words
+def get_Characters(X):
+    characters = []
+    for word in X:
+        for char in word:
+            characters.append(char)
+    return np.array(characters)
+
+
+#returns words from characters
+def get_Words(X, y):
+    words = [[]]
+    cnt = 0
+    for idx, word in enumerate(y):
+        length = len(word)
+        for _ in range(length):
+            words[idx].append(X[cnt])
+            cnt += 1
+        words.append([])
+    return words
+
+#rotates an image by theta degrees
+
+def rotate_image(image, theta):
+
+    image = image.reshape((16, 8))
+    transformed_image = scipy.ndimage.interpolation.rotate(image, angle=float(theta))
+    delta_height = int((transformed_image.shape[0] + 1 - image.shape[0]) // 2)
+    delta_width = int((transformed_image.shape[1] + 1 - image.shape[1]) // 2)
+
+    transformed_image = transformed_image[delta_height:delta_height + image.shape[0], delta_width: delta_width + image.shape[1]]
+
+    loc = np.where(transformed_image == 0)
+    transformed_image[loc] = image[loc]
+
+    return transformed_image
+
+#returns the target value of an image
+def translate_image(image, offsets):
+    image = image.reshape((16, 8))
+    target_value = image
+    delta_height, delta_width = int(offsets[0]), int(offsets[1])
+
+    target_value[max(0, delta_height): min(image.shape[0], image.shape[0] + delta_height),
+    max(0, delta_width): min(image.shape[1], image.shape[1] + delta_width)] = image[max(0, 1 - delta_height): min(image.shape[0], image.shape[0] - delta_height),
+                                                          max(0, 1 - delta_width): min(image.shape[1], image.shape[1] - delta_width)]
+
+    target_value[delta_height: image.shape[0], delta_width: image.shape[1]] = image[0: image.shape[0] - delta_height, 0: image.shape[1] - delta_width]
+
+    return target_value
 
 # train_data, labels = read_for_crf('train.txt')
 # print(train_data, labels)
