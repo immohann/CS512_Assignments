55bea14da6488aefbfa4852ef3ce20e12278af08
immohan <mohanqwerty5@gmail.com>
2022-03-02 23:48:51 -0500

    updated

--- code/all_functions.py
+++ code/all_functions.py
@@ -3,8 +3,14 @@ import time
 from scipy.optimize import check_grad
 import os
 
+#try either of these
+# path = os.getcwd()
+
 path = os.path.dirname(os.getcwd())
 
+global_counter = 0
+
+#returns trained model parameters
 def load_model_txt(filename):
     w_t = []
     with open(filename, 'r') as f:
@@ -13,6 +19,8 @@ def load_model_txt(filename):
 
     return np.array(w_t)
 
+
+#matricizes W and T
 def extract_W_T(w_t_list):
     W = np.zeros((26, 128))
     T = np.zeros((26, 26))
@@ -27,230 +35,236 @@ def extract_W_T(w_t_list):
     
     return W,T
 
+# to calculate alpha, considering from the lecture, alpha fwd and beta backward
+def calculate_fwd_bkwd_msg(x, w, t):
+    # initializing
+    w_x = np.dot(x, w.T)
+    t_transpose = t.transpose()
+    length_word = len(w_x)
+    forward_result = np.zeros((length_word, 26))
 
-def compute_forward_message(x, w, t):
-    w_x = np.dot(x,w.T)
-    num_words = len(w_x)
-    M = np.zeros((num_words, 26))
-
-    # iterate through all characters in each word
-    for i in range(1, num_words):
-        alpha = M[i - 1] + t.transpose()
+    # looping the characters in each word
+    for i in range(1, length_word):
+        alpha = forward_result[i - 1] + t_transpose
         alpha_max = np.max(alpha, axis=1)
-        # prepare V - V.max()
-        alpha = (alpha.transpose() - alpha_max).transpose()
-        M[i] = alpha_max + np.log(np.sum(np.exp(alpha + w_x[i - 1]), axis=1))
+        alpha_transposed = alpha.transpose()
+        alpha = alpha_transposed - alpha_max
+        alpha = alpha.transpose()
+        r = np.log(np.sum(np.exp(alpha + w_x[i - 1]), axis=1))
+        forward_result[i] = alpha_max + r
 
-    return M
+    # get the index of the last letter of the word
+    last_index = len(w_x) - 1
+    backward_result = np.zeros((len(w_x), 26))
 
-
-def compute_backward_message(x, w, t):
-    # get the index of the final letter of the word
-    w_x = np.dot(x,w.T)
-    fin_index = len(w_x) - 1
-    M = np.zeros((len(w_x), 26))
-
-    for i in range(fin_index - 1, -1, -1):
-        beta = M[i + 1] + t
+    # reversing the array and looping from the reverse
+    for i in range(last_index - 1, -1, -1):
+        beta = backward_result[i + 1] + t
         beta_max = np.max(beta, axis=1)
-        # prepare V - V.max()
-        beta = (beta.transpose() - beta_max).transpose()
-        M[i] = beta_max + np.log(np.sum(np.exp(beta + w_x[i + 1]), axis=1))
-
-    return M
-
-
-def compute_numerator(y, x, w, t):
-    w_x = np.dot(x,w.T)
-    sum_ = 0
-    # for every word
-    for i in range(len(w_x)):
-        sum_ += w_x[i][y[i]]
-
-        if (i > 0):
-            # t stored as T{current, prev}
-            sum_ += t[y[i - 1]][y[i]]
-
-    return np.exp(sum_)
+        beta_transposed = beta.transpose()
+        beta = beta_transposed - beta_max
+        beta = beta.transpose()
+        r = np.log(np.sum(np.exp(beta + w_x[i + 1]), axis=1))
+        backward_result[i] = beta_max + r
 
 
-def compute_denominator(alpha, x, w):
-    # forward propagate to the end of the word and return the sum
-    return np.sum(np.exp(alpha[-1] + np.dot(x,w.T)[-1]))
+    return forward_result, backward_result
 
 
-def compute_gradient_wrt_Wy(X, y, w, t, alpha, beta, denominator):
-    gradient = np.zeros((26, 128))
+def calc_nmrtr(y, x, w, t):
+    #initialization
+    result = 0
+    w_dot_x = np.dot(x,w.T)
 
-    w_x = np.dot(X,w.T)
+    for i in range(len(w_dot_x)):
+        result = result + w_dot_x[i][y[i]]
+        if i > 0:
+            result = result + t[y[i-1]][y[i]]
 
-    for i in range(len(X)):
-        gradient[y[i]] += X[i]
-
-        # for each position, reduce the probability of the character
-        temp = np.ones((26, 128)) * X[i]
-        temp = temp.transpose()
-        temp = temp * np.exp(alpha[i] + beta[i] + w_x[i]) / denominator
-
-        gradient -= temp.transpose()
-
-    return gradient.flatten()
+    res_exp = np.exp(result)
+    return res_exp
 
 
-def compute_gradient_wrt_Tij(y, x, w, t, alpha, beta, denominator):
-    gradient = np.zeros(26 * 26)
-    w_x = np.dot(x,w.T)
-    for i in range(len(w_x) - 1):
+def calculate_denom(a, x, w):
+    l = a[-1]
+    r = np.dot(x,w.T)[-1]
+    res = np.exp(l+r)
+    return np.sum(res)
+
+
+def compute_gradient_wrt_Wy(X, y, w, t, alpha, beta, dnm):
+    len_x = len(X)
+    w_x = np.dot(X, w.T)
+    grad = np.zeros((26, 128))
+
+    for i in range(len_x):
+        result = (np.ones((26, 128)) * X[i] ).transpose()
+        grad[y[i]] = grad[y[i]] + X[i]
+        result = result*np.exp(alpha[i] + beta[i] + w_x[i])
+        result /= dnm
+        grad = grad - result.transpose()
+
+    grad = grad.flatten()
+    return grad
+
+
+def compute_gradient_wrt_Tij(y, x, w, t, alpha, beta, dnm):
+    w_x = np.dot(x, w.T)
+    grad = np.zeros(26 * 26)
+
+    # calculate_the_gradients
+    for i in range(len(w_x)-1):
         for j in range(26):
-            gradient[j * 26: (j + 1) * 26] -= np.exp(w_x[i] + t.transpose()[j] + w_x[i + 1][j] + beta[i + 1][j] + alpha[i])
+            sum = w_x[i] + t.transpose()[j] + w_x[i+1][j] + beta[i+1][j] + alpha[i]
+            e_sum = np.exp(sum)
+            grad[j*26:(j+1)*26] = grad[j*26:(j+1)*26] - e_sum
 
-    # normalize the gradient
-    gradient /= denominator
+    # dividing to normalize
+    grad = grad/dnm
 
-    # add the gradient for the next word
-    for i in range(len(w_x) - 1):
-        t_index = y[i]
-        t_index += 26 * y[i + 1]
-        gradient[t_index] += 1
+    for i in range(len(w_x)-1):
+        t_i = y[i] + 26*y[i+1]
+        grad[t_i] = grad[t_i] + 1
 
-    return gradient
+    return grad
 
-def gradient_word(X, y, w, t, word_index):
-    # O(n * |Y|)
-    # w_x = np.dot(X[word_index], w.T)
-    # O(n * |Y|^2)
-    f_mess = compute_forward_message(X[word_index], w, t)
-    # O(n * |Y|^2)
-    b_mess = compute_backward_message(X[word_index], w, t)
-    # O(1)
-    den = compute_denominator(f_mess, X[word_index], w)
-    # O(n * |Y|^2)
-    wy_grad = compute_gradient_wrt_Wy(X[word_index], y[word_index], w, t, f_mess, b_mess, den)
-    # O(n * |Y|^2)
-    t_grad = compute_gradient_wrt_Tij(y[word_index], X[word_index], w, t, f_mess, b_mess, den)
-    return np.concatenate((wy_grad, t_grad))
+def grad_word(X, y, w, t, word_i):
+    forward_message, backward_message = calculate_fwd_bkwd_msg(X[word_i], w, t)
+    den = calculate_denom(forward_message, X[word_i],w)
+    grad_wrt_w = compute_gradient_wrt_Wy(X[word_i],y[word_i],w,t,forward_message,backward_message,den)
+    grad_wrt_t = compute_gradient_wrt_Tij(y[word_i],X[word_i],w,t,forward_message,backward_message,den)
+    return np.concatenate((grad_wrt_w, grad_wrt_t))
 
-def compute_log_p_y_given_x(x,w, y, t, word_index):
-    f_mess = compute_forward_message(x, w, t)
-    return np.log(compute_numerator(y, x, w, t) / compute_denominator(f_mess, x, w))
+def calculate_log_py_gvn_x(x, w, y, t):
+    f_mess, b_mess = calculate_fwd_bkwd_msg(x, w, t)
+    return np.log(calc_nmrtr(y, x, w, t) / calculate_denom(f_mess, x, w))
 
 
-def compute_log_p_y_given_x_avg(w_t_list, X, y, limit):
-
+def calc_log_py_gvn_xavg(w_t_list, X, y, num_examples):
+    sum = 0
     w,t = extract_W_T(w_t_list)
+    # print(w,t)
+    # print(w.shape)
 
-    total = 0
-    for i in range(limit):
-        # w_x = np.dot(X[i], w)
-        total += compute_log_p_y_given_x(X[i],w, y[i], t, i)
+    for i in range(num_examples):
+        r = calculate_log_py_gvn_x(X[i], w, y[i], t)
+        sum = sum + r
 
-    return total / (limit)
+    return sum / (num_examples)
 
-def averaged_gradient(w_t_list, X, y, limit):
-    
+def averaged_gradient(w_t_list, X, y, len_x):
     w,t = extract_W_T(w_t_list)
+    #print(w,t)
+    total = np.zeros(128*26+26**2)
+    #print(w.shape, t.shape)
 
-    total = np.zeros(128 * 26 + 26 ** 2)
-    for i in range(limit):
-        total += gradient_word(X, y, w, t, i)
+    for i in range(len_x):
+        res = grad_word(X, y, w, t, i)
+        #print(res, res.shape)
+        total = total + res
     # print(total / (limit))
-    return total / (limit)
+    return total / (len_x)
 
 def check_gradient(w_t_list, X, y):
-    # check the gradient of the first 10 words
-    grad_value = check_grad(compute_log_p_y_given_x_avg, averaged_gradient, w_t_list,  X, y, 10)
-    print("Gradient check (first 10 characters) : ", grad_value)
+    # gradient of first 20 words
+    grad_value = check_grad(calc_log_py_gvn_xavg, averaged_gradient, w_t_list, X, y, 20)
+    print("Gradient limited to 20 words: ", grad_value)
 
 
-def crf_obj(params, X, y, C):
-    num_examples = len(X)
-    l2_regularization = 1 / 2 * np.sum(params ** 2)
-    log_loss = compute_log_p_y_given_x_avg(params, X, y, num_examples)
-    return -C * log_loss + l2_regularization
+def crf_obj(w_t, X, y, C):
+    log_loss = calc_log_py_gvn_xavg(w_t, X, y, len(X))
+    # log loss + regularizedloss
+    result = -C*log_loss+ 1/2*np.sum(w_t**2)
+    return result
 
-def crf_obj_gradient(params, X, y, C):
-    num_examples = len(X)
-    logloss_gradient = averaged_gradient(params, X, y, num_examples)
-    l2_loss_gradient = params
-    return -C * logloss_gradient + l2_loss_gradient
+def crf_obj_gradient(w_t, X, y, C):
+    global global_counter
+    global_counter = global_counter + 1
+    ll_grad = averaged_gradient(w_t, X, y, len(X))
+    print("Gradient evaluation counter - ",global_counter)
+    return -C*ll_grad+w_t
 
 
-def compute_word_char_accuracy_score(y_preds, y_true):
-    word_count = 0
-    correct_word_count = 0
-    letter_count = 0
-    correct_letter_count = 0
+def calc_word_char_acc(y_preds, ground_truth):
+    # Intitializing word/letter counts
+    count, c_count, count_l, c_count_l = 0, 0, 0, 0
 
-    for y_t, y_p in zip(y_true, y_preds):
-        word_count += 1
-        if np.array_equal(y_t, y_p):
-            correct_word_count += 1
+    # Iterating through the predicted and actual values
+    for y, y_hat in zip(ground_truth, y_preds):
+        count = count + 1
+        count_l = count_l + len(y_hat)
+        c_count_l = c_count_l + np.sum(y_hat == y)
+        if np.array_equal(y, y_hat):
+            c_count = c_count + 1
 
-        letter_count += len(y_p)
-        correct_letter_count += np.sum(y_p == y_t)
+    word_ratio = c_count / count
+    letter_ratio = c_count_l / count_l
+    return word_ratio, letter_ratio
 
-    return correct_word_count / word_count, correct_letter_count / letter_count
+def compute_energies(X, w, t): ####
+    # acquiring the matrix shape
+    result = np.zeros((len(X), 26))
+    result[0] = np.inner(X[0], w)
 
-def get_energies(X, w, t):
-    # for 100 letters I need an M matrix of 100  X 26
-    M = np.zeros((len(X), 26))
-
-    # populates first row
-    M[0] = np.inner(X[0], w)
-
-    # go row wise through M matrix, starting at line 2 since first line is populated
     for row in range(1, len(X)):
-
-        # go column wise, populating the best sum of the previous + T[previous letter][
-        for cur_letter in range(26):
-            # initialize with giant negative number
-            best = -np.inf
-
-            # iterate over all values of the previous letter, fixing the current letter
+        for letter in range(26):
+            neg_num = -10000
             for prev_letter in range(26):
-                temp_product = M[row - 1][prev_letter] + np.inner(X[row], w[cur_letter]) + t[prev_letter][cur_letter]
-                if (temp_product > best):
-                    best = temp_product
-            M[row][cur_letter] = best
-    return M
+                a = np.inner(X[row], w[letter])
+                t_r = result[row - 1][prev_letter] + t[prev_letter][letter] + a
+                if t_r > neg_num:
+                    neg_num = t_r
+            result[row][letter] = neg_num
+    return result
     
-def decode_crf_word(X, w, t):
-    M = get_energies(X, w, t)
+#....
+#get the max from energies
+def get_word(X, w, t): ####
+    #val = 1e-6
+    val = 1e-5
+    result_from_e = compute_energies(X, w, t)
+    #print("here")
+    position = len(result_from_e)-1
+    #print(position)
+    previous_position = position-1
+    letter = np.argmax(result_from_e[position]) #get the max from energies
+    cur_val = result_from_e[position][letter]
+    result = [letter]
 
-    cur_word_pos = len(M) - 1
-    prev_word_pos = cur_word_pos - 1
-
-    cur_letter = np.argmax(M[cur_word_pos])
-    cur_val = M[cur_word_pos][cur_letter]
-
-    solution = [cur_letter]
-
-    while (cur_word_pos > 0):
-        for prev_letter in range(26):
-            energy = np.inner(X[cur_word_pos], w[cur_letter])
-            if (np.isclose(cur_val - M[prev_word_pos][prev_letter] - t[prev_letter][cur_letter] - energy, 0,
-                           rtol=1e-5)):
-                solution.append(prev_letter)
-                cur_letter = prev_letter
-                cur_word_pos -= 1
-                prev_word_pos -= 1
-                cur_val = M[cur_word_pos][cur_letter]
+    while position:
+        for prev in range(26):
+            e = np.inner(X[position], w[letter])
+            flag = np.isclose(cur_val - result_from_e[previous_position][prev] - t[prev][letter] - e, 0, rtol=val)
+            if flag:
+                # print("now here")
+                letter = prev
+                position = position-1
+                previous_position = previous_position-1
+                cur_val = result_from_e[position][letter]
+                result.append(prev)
                 break
 
-    solution = solution[::-1]  # reverse the prediction string
-    return np.array(solution)
+    result = result[::-1]
+    to_return = np.array(result)
+    return to_return
 
 
-def decode_crf(X, w, t):
-    y_pred = []
-    for i, x in enumerate(X):
-        preds = decode_crf_word(x, w, t)
-        y_pred.append(preds)
-    return y_pred
+def decode_words_and_get_preditcions(X, w, t):
+    y_hat = []
+
+    for x in X:
+        #print(x)
+        word_pred = get_word(x, w, t)
+
+        #print(word_pred), "here")
+        #print(word_pred.shape)
+        y_hat.append(word_pred)
+
+    return y_hat
 
 
-def crf_test(model, X_test, y_test,model_name):
-    print("Function value: ", crf_obj(model, X_test, y_test, C=1000))
+def crf_test(model, X_test, y_test,model_name, C):
+    print("Function value: ", crf_obj(model, X_test, y_test, C))
 
     ''' accuracy '''
 
@@ -262,7 +276,7 @@ def crf_test(model, X_test, y_test,model_name):
             text_file.write(str(elt) + "\n")
 
     #x_test = convert_word_to_character_dataset(X_test)
-    y_preds = decode_crf(X_test, w, t)
+    y_preds = decode_words_and_get_preditcions(X_test, w, t)
     #y_preds = convert_character_to_word_dataset(y_preds, y_test)
 
     with open(path + "/result/prediction.txt", "w") as text_file:
@@ -272,7 +286,89 @@ def crf_test(model, X_test, y_test,model_name):
                 text_file.write(str(word + 1))
                 text_file.write("\n")
 
-    accuracy =  compute_word_char_accuracy_score(y_preds, y_test)
+    accuracy = calc_word_char_acc(y_preds, y_test)
     print("Test accuracy : ",accuracy)
 
     return accuracy
+
+#function for evaluation of linear SVM performance
+def linear_SVM_performance(y_target, y_predicted, target_ids):
+    target_words, predicted_words = [], []
+    lastWord = -1000
+    character_truepositives, word_truepositives = 0, 0
+
+    for i, (target, predicted) in enumerate(zip(y_target, y_predicted)):
+        target_word = int(target_ids[i])
+
+        if target_word == lastWord:
+            target_words[-1].append(target)
+            predicted_words[-1].append(predicted)
+        else:
+            target_words.append([target])
+            predicted_words.append([predicted])
+            lastWord = target_word
+
+
+    for target_character,predicted_character in zip(y_target, y_predicted):
+        if target_character == predicted_character:
+            character_truepositives += 1
+
+    Character_Accuracy = float(character_truepositives)/float(y_target.shape[0])
+
+    for target_word,predicted_word in zip(target_words, predicted_words):
+        if np.array_equal(target_word, predicted_word):
+            word_truepositives += 1
+
+    word_Accuracy = float(word_truepositives)/float(len(target_words))
+
+    print("Letter-wise prediction Accuracy: %0.3f" %(Character_Accuracy))
+    print("Word-wise prediction Accuracy: %0.3f" %(word_Accuracy))
+
+    return Character_Accuracy, word_Accuracy
+
+
+#function for evaluation of Structured SVM performance
+def structuredSVM_performance(file_target, file_predicted):
+
+    with open(file_target, 'r') as file_target, open(file_predicted, 'r') as file_predicted:
+        target_words, predicted_words, target_characters, predicted_characters = [], [], [], []
+
+        lastWord = -1000
+        character_truepositives, word_truepositives = 0, 0
+
+        for target, predicted in zip(file_target, file_predicted):
+            targets = target.split()
+            target_character = int(targets[0])
+            target_characters.append(target_character)
+
+            predicted_character = int(predicted)
+
+            if hasattr(predicted_character, 'len') > 0:
+                predicted_character = predicted_character[0]
+
+            predicted_characters.append(predicted_character)
+            target_Word = int(targets[1][4:])
+
+            if target_Word == lastWord:
+                target_words[-1].append(target_character)
+                predicted_words[-1].append(predicted_character)
+            else:
+                target_words.append([target_character])
+                predicted_words.append([predicted_character])
+                lastWord = target_Word
+
+        for target_character, predicted_character in zip(target_characters, predicted_characters):
+            if target_character == predicted_character:
+                character_truepositives += 1
+
+        for target_Word, pred_word in zip(target_words, predicted_words):
+            if np.array_equal(target_Word, pred_word):
+                word_truepositives += 1
+
+        character_accuracy = float(character_truepositives) / float(len(target_characters))
+        word_accuracy = float(word_truepositives) / float(len(target_words))
+
+        print("Character level accuracy : %0.3f" % (character_accuracy))
+        print("Word level accuracy : %0.3f" % (word_accuracy))
+
+        return character_accuracy, word_accuracy
--- code/ref_optimize.py
+++ code/ref_optimize.py
@@ -1,12 +1,19 @@
 # Imports
 import numpy as np
 import os
+from subprocess import run, PIPE
+import matplotlib.pyplot as plt
+from sklearn.svm import LinearSVC
+import pickle
 
 import scipy.optimize as opt
-from read_data import read_for_crf, prepare_structured_dataset
+from read_data import read_for_crf, prepare_structured_dataset, crf_dataLoader, linearSVM_dataLoader, get_Words, get_Characters
 from all_functions import *
 
+fmin_tnc_results = {}
 path = os.path.dirname(os.getcwd())
+
+
 
 #for 2a   #Train data to calculate the gradient on
 X_gradient_compute, y_gradient_compute = read_for_crf(path + "/data/train.txt")
@@ -20,9 +27,6 @@ X_train, y_train = prepare_structured_dataset(path + "/data/train_struct.txt")
 X_test, y_test = prepare_structured_dataset(path + "/data/train_struct.txt")
 
 def gradient_compute(X_train, y_train,W_T_matrix):
-    """Compute the CRF objective and gradient on the list of words (word_list)
-    evaluated at the current model x (w_y and T, stored as a vector)
-    """
     # print(path)
 
  
@@ -34,37 +38,38 @@ def gradient_compute(X_train, y_train,W_T_matrix):
     print("Total time:", time.time() - start)
 
 
-    with open("gradient.txt", "w") as text_file:
+    with open(path + "/result/gradient.txt", "w") as text_file:
         for i, elt in enumerate(average_gradient):
             text_file.write(str(elt))
             text_file.write("\n")
 
-    report_value = compute_log_p_y_given_x_avg(W_T_matrix, X_train, y_train, len(X_train))
+    report_value = calc_log_py_gvn_xavg(W_T_matrix, X_train, y_train, len(X_train))
     print("This is the value to be reported for 2.a",report_value)
 
     return report_value
 
-#crf_obj  - train_crf ttitto
-#xo is params
+
 def ref_optimize(x0, X_train, y_train,X_test, y_test , c, model_name):
-    print("Optimizing parameters. This will take a long time (at least 1 hour per model).")
+    print("Optimizing w and T. Started.")
 
     start = time.time()
-    result = opt.fmin_tnc(crf_obj, x0, crf_obj_gradient, (X_train, y_train, c), disp=1)[0]
-    print("Total time: ", end='')
+    result = opt.fmin_tnc(crf_obj, x0, crf_obj_gradient, (X_train, y_train, c), disp=1)
+    print("Time taken: ", end='')
     print(time.time() - start)
 
     model = result[0]
-
-    accuracy = crf_test(model, X_test, y_test, model_name)
-    print('CRF test accuracy for c = {}: {}'.format(c, accuracy))
+    fmin_tnc_results[c] = model
+    accuracy = crf_test(model, X_test, y_test, model_name, c)
+    print('Accuracy for c = {}: {}'.format(c, accuracy))
     return accuracy
 
 #2a
-gradient_compute(X_gradient_compute, y_gradient_compute,W_T_matrix)
-#2b
-ref_optimize(W_T_matrix, X_train, y_train,X_test, y_test , c=1000, model_name='new')
+if __name__ == '__main__':
+    gradient_compute(X_gradient_compute, y_gradient_compute,W_T_matrix)
 
+#2b
+    for c in [5]:
+        ref_optimize(W_T_matrix, X_train, y_train,X_test, y_test , c=c, model_name='c_equal_'+ str(c))
 
 
     
