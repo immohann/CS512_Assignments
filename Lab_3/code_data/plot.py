b76ac9f0a49faa958e76261e4d649c54ae0f0a28
Harsh Mishra <mishraharsh169@gmail.com>
2022-04-18 23:14:17 -0500

    added plots

--- Lab_3/code_data/plot.py
+++ Lab_3/code_data/plot.py
@@ -14,24 +14,24 @@ train_accuracy_plain = []
 adv_model_test_acc = []
 prox_model_test_acc = []
 
-with open(path + "BasicModel_train.txt", "r") as f:
-  for line in f:
-    train_accuracy_plain.append(float(line.strip()))
+# with open(path + "BasicModel_train.txt", "r") as f:
+#   for line in f:
+#     train_accuracy_plain.append(float(line.strip()))
 
-with open(path + "BasicModel_test.txt", "r") as f:
-  for line in f:
-    test_accuracy_plain.append(float(line.strip()))
+# with open(path + "BasicModel_test.txt", "r") as f:
+#   for line in f:
+#     test_accuracy_plain.append(float(line.strip()))
 
-iterations = [x for x in range(plain_model_epoch)]
+# iterations = [x for x in range(plain_model_epoch)]
 
-plt.figure(figsize=(12,10))
-plt.plot(iterations, train_accuracy_plain, color = 'purple', linewidth = 2, label='Train')
-plt.plot(iterations, test_accuracy_plain, color = 'red', linewidth = 2, label='Test')
-plt.legend(loc = "upper left")
-plt.xlabel("Epochs")
-plt.ylabel("Accuracy")
-plt.title("Accuracy vs Epochs for Plain Model.")
-plt.savefig(path + 'train_test_plain_model.png')
+# plt.figure(figsize=(12,10))
+# plt.plot(iterations, train_accuracy_plain, color = 'purple', linewidth = 2, label='Train')
+# plt.plot(iterations, test_accuracy_plain, color = 'red', linewidth = 2, label='Test')
+# plt.legend(loc = "upper left")
+# plt.xlabel("Epochs")
+# plt.ylabel("Accuracy")
+# plt.title("Accuracy vs Epochs for Plain Model.")
+# plt.savefig(path + 'train_test_plain_model.png')
 
 
 #For question 4
@@ -45,20 +45,21 @@ with open(path + "ProxModel_acc.txt", "r") as f:
     prox_model_test_acc.append(float(line.strip()))
 
 
-def plot_for_q4_5(epoch, epoch_epsilon, ep_opt, test_opt, test_ep1, test_ep2, test_ep3, ep1, ep2, ep3, file_name, question):
+def plot_for_q4_5(epoch, short_epoch, test_ep0, test_ep1, test_ep2, test_ep3, test_ep4, ep0,ep1, ep2, ep3,ep4, file_name, question):
 
-    x= [x for x in range(epoch)]
-    x_ep = [x_ep for x_ep in range(epoch_epsilon)]
+    epoch= [x for x in range(epoch)]
+    short_epoch = [x_ep for x_ep in range(short_epoch)]
 
     path = "../Data/"
     
 
     plt.figure(figsize=(12,10))
-    plt.plot(x, test_opt, color='purple', linewidth=2, label="Optimal Epsilon = " + str(ep_opt))
-    plt.plot(x_ep, test_ep1, color = 'red', linewidth = 2, label=ep1)
-    plt.plot(x_ep, test_ep2, color = 'blue', linewidth = 2, label=ep2)
-    plt.plot(x_ep, test_ep3, color = 'black', linewidth = 2, label=ep3)
-    plt.legend(loc = "upper left")
+    plt.plot(epoch, test_ep0, color='purple', linewidth=2, label="Optimal Epsilon = " + str(ep0))
+    plt.plot(short_epoch, test_ep1, color = 'red', linewidth = 2, label=ep1)
+    plt.plot(short_epoch, test_ep2, color = 'blue', linewidth = 2, label=ep2)
+    plt.plot(short_epoch, test_ep3, color = 'black', linewidth = 2, label=ep3)
+    plt.plot(short_epoch, test_ep4, color = 'green', linewidth = 2, label=ep4)
+    plt.legend(loc = "lower right")
 
     plt.xlabel("Epochs")
     plt.ylabel("Test Accuracy")
@@ -70,11 +71,15 @@ def plot_for_q4_5(epoch, epoch_epsilon, ep_opt, test_opt, test_ep1, test_ep2, te
  
     plt.savefig(path + file_name)
 
+# [0.01, 0.05, 0.1, 1.0, 5.0]
 
-plot_for_q4_5(epoch = Adv_epoch, epoch_epsilon = 50, ep_opt = 0.08, test_opt = adv_model_test_acc[:100], 
-    test_ep1 = adv_model_test_acc[100:100 + 50], test_ep2 = adv_model_test_acc[200:200 + 50], test_ep3 = adv_model_test_acc[300:300+50], 
-    ep1 = 0.01, ep2 = 0.1, ep3 = 1.0, file_name = 'Accuracy_AdvModel.png', question= 4)
 
-plot_for_q4_5(epoch = Prox_epoch, epoch_epsilon = 50, ep_opt = 0.08, test_opt = prox_model_test_acc[:100], 
-    test_ep1 = prox_model_test_acc[100:100 + 50], test_ep2 = prox_model_test_acc[200:200 + 50], test_ep3 = prox_model_test_acc[300:300+50], 
-    ep1 = 0.1, ep2 = 1.0, ep3 = 5.0, file_name = 'Accuracy_ProxModel.png', question= 5)
+# plot_for_q4_5(epoch = Adv_epoch, short_epoch = 50, test_ep0 = adv_model_test_acc[:50], 
+#     test_ep1 = adv_model_test_acc[100:100 + 100], test_ep2 = adv_model_test_acc[200:200 + 50], test_ep3 = adv_model_test_acc[300:300+50],
+#     test_ep4 = adv_model_test_acc[400:400+50], 
+#     ep0 = 0.01, ep1 = 0.05, ep2 = 0.1, ep3 = 1.0,ep4 = 5.0, file_name = 'Accuracy_AdvModel.png', question= 4)
+
+plot_for_q4_5(epoch = Prox_epoch, short_epoch = 50, test_ep0 = prox_model_test_acc[:100], 
+    test_ep1 = prox_model_test_acc[100:100 + 50], test_ep2 = prox_model_test_acc[200:200 + 50], test_ep3 = prox_model_test_acc[300:300+50],
+    test_ep4 = prox_model_test_acc[400:400+50], 
+    ep0 = 0.01, ep1 = 0.05, ep2 = 0.1, ep3 = 1.0,ep4 = 5.0, file_name = 'Accuracy_ProxModel.png', question= 5)
